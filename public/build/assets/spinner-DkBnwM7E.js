039902144c2413c8f0713d5f07983d8249ce6db9
Timothe Portelli <portelli.timothe@gmail.com>
2026-06-01 07:36:31 +0200

    Ajout des assets build pour prod

---
+++ public/build/assets/spinner-DkBnwM7E.js
@@ -0,0 +1,1 @@
+import{j as s}from"./app-D5C-PbUC.js";import{c as i}from"./button-BghsFFRM.js";import{L as o}from"./loader-circle-KTgwkw6h.js";function m({className:r,...a}){return s.jsx(o,{role:"status","aria-label":"Loading",className:i("size-4 animate-spin",r),...a})}export{m as S};
