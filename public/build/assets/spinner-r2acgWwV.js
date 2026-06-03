d6e98de859da71fad7955b4e6145f7970a8c3405
Timothe Portelli <portelli.timothe@gmail.com>
2026-06-03 19:47:06 +0200

    npm run build

--- public/build/assets/spinner-r2acgWwV.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./app-bGvOBADs.js";import{c as i}from"./button-DT-UqLS1.js";import{L as o}from"./loader-circle-EBVZr-na.js";function m({className:r,...a}){return s.jsx(o,{role:"status","aria-label":"Loading",className:i("size-4 animate-spin",r),...a})}export{m as S};
